5b1cd9e533ea3cb74057aea8487c17823b67439d
Nikita <alyshiev.nikita@gmail.com>
2022-02-23 14:52:20 +0400

    add activiti and background

--- app/src/main/java/com/example/english/MainActivity.java
+++ app/src/main/java/com/example/english/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.english;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button ButtonEa = findViewById(R.id.buttonEa);
+        Button ButtonHa = findViewById(R.id.buttonHa);
+
+
+        ButtonEa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        ButtonHa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
+
+
+
+
 }
